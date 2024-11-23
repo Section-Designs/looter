c34a823323a8e7c45762e5262590f5d5cddc3584
miles <roleplaysboys@gmail.com>
2024-11-23 09:12:04 +0200

    update logger.js

--- util/logger.js
+++ util/logger.js
@@ -1,14 +1,23 @@
-const chalk = require("chalk");
 const moment = require("moment");
 
 module.exports.Logger = class Logger {
+  /**
+   * Apply color to a string
+   * @param {string} colorCode - The ANSI color code
+   * @param {string} text - The text to color
+   * @returns {string}
+   */
+  static colorize(colorCode, text) {
+    return `\x1b[${colorCode}m${text}\x1b[0m`;
+  }
+
   /**
    * Log an info message
    * @param {string} message
    */
   static info(message) {
     console.log(
-      `${chalk.blueBright("[INFO]")} ${moment().format("HH:mm:ss")} ${message}`
+      `${this.colorize(94, "[INFO]")} ${moment().format("HH:mm:ss")} ${message}`
     );
   }
 
@@ -18,9 +27,7 @@ module.exports.Logger = class Logger {
    */
   static warn(message) {
     console.log(
-      `${chalk.yellowBright("[WARN]")} ${moment().format(
-        "HH:mm:ss"
-      )} ${message}`
+      `${this.colorize(93, "[WARN]")} ${moment().format("HH:mm:ss")} ${message}`
     );
   }
 
@@ -30,7 +37,7 @@ module.exports.Logger = class Logger {
    */
   static error(message) {
     console.log(
-      `${chalk.redBright("[ERROR]")} ${moment().format("HH:mm:ss")} ${message}`
+      `${this.colorize(91, "[ERROR]")} ${moment().format("HH:mm:ss")} ${message}`
     );
   }
 
@@ -40,7 +47,7 @@ module.exports.Logger = class Logger {
    */
   static success(message) {
     console.log(
-      `${chalk.greenBright("[SUCCESS]")} ${moment().format(
+      `${this.colorize(92, "[SUCCESS]")} ${moment().format(
         "HH:mm:ss"
       )} ${message}`
     );
@@ -52,7 +59,7 @@ module.exports.Logger = class Logger {
    */
   static debug(message) {
     console.log(
-      `${chalk.magentaBright("[DEBUG]")} ${moment().format(
+      `${this.colorize(95, "[DEBUG]")} ${moment().format(
         "HH:mm:ss"
       )} ${message}`
     );
